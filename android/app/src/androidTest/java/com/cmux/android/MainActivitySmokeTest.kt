707275ead3eb9c590b2733ce51f77b2e8792d10f
ihasit <58540016+ihasit@users.noreply.github.com>
2026-07-08 19:04:20 +0800

    Add Android WebView auth instrumentation test

--- android/app/src/androidTest/java/com/cmux/android/MainActivitySmokeTest.kt
+++ android/app/src/androidTest/java/com/cmux/android/MainActivitySmokeTest.kt
@@ -1,18 +1,33 @@
 package com.cmux.android
 
+import android.content.Context
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
 import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.clearElement
 import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
 import androidx.test.espresso.web.webdriver.DriverAtoms.getText
+import androidx.test.espresso.web.webdriver.DriverAtoms.webClick
+import androidx.test.espresso.web.webdriver.DriverAtoms.webKeys
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.containsString
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivitySmokeTest {
+    @Before
+    fun clearState() {
+        ApplicationProvider.getApplicationContext<Context>()
+            .getSharedPreferences("cmux_mobile", Context.MODE_PRIVATE)
+            .edit()
+            .clear()
+            .commit()
+    }
+
     @Test
     fun launchShowsPairingAndStackAuthControls() {
         ActivityScenario.launch(MainActivity::class.java).use {
@@ -23,6 +38,40 @@ class MainActivitySmokeTest {
             onWebView()
                 .withElement(findElement(Locator.ID, "authTitle"))
                 .check(webMatches(getText(), containsString("Stack Auth token")))
+        }
+    }
+
+    @Test
+    fun stackAccessTokenCanBeSavedAndClearedThroughWebViewBridge() {
+        ActivityScenario.launch(MainActivity::class.java).use {
+            onWebView()
+                .withElement(findElement(Locator.ID, "stackAccessToken"))
+                .perform(clearElement())
+                .perform(webKeys("instrumentation-access-token"))
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "saveStackAccessToken"))
+                .perform(webClick())
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "authStatusText"))
+                .check(webMatches(getText(), containsString("Stack access token is configured")))
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "toast"))
+                .check(webMatches(getText(), containsString("Stack access token saved")))
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "clearStackAccessToken"))
+                .perform(webClick())
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "authStatusText"))
+                .check(webMatches(getText(), containsString("Add a Stack access token")))
+
+            onWebView()
+                .withElement(findElement(Locator.ID, "toast"))
+                .check(webMatches(getText(), containsString("Stack access token cleared")))
         }
     }
 }
